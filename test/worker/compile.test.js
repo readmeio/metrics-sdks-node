a07c30a506c8257a68012f206de23d38ad0d43c9
Dom Harrington <dom@harrington-mail.com>
2018-09-13 10:16:53 -0700

    Getting webpack compilation working

--- test/worker/compile.test.js
+++ test/worker/compile.test.js
@@ -12,13 +12,13 @@ const template = fs.readFileSync(path.join(__dirname, '../../worker/template.js'
 describe('compile-worker', () => {
   it('should replace VERSION with the package.json version', async () => {
     const output = await compileWorker(template);
-    assert.equal(output.indexOf('VERSION') === -1, true);
+    assert.equal(output.indexOf('version = VERSION') === -1, true);
     assert.equal(output.indexOf(version) > -1, true);
   });
 
   it('should replace HOST with the config.host', async () => {
     const output = await compileWorker(template);
-    assert.equal(output.indexOf('HOST') === -1, true);
+    assert.equal(output.indexOf('host = HOST') === -1, true);
     assert.equal(output.indexOf(config.host) > -1, true);
   });
 });
