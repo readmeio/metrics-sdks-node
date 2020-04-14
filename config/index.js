5a33bae82ce08593be159cec99fda7d37d4871f3
Kanad Gupta <kgupta@umn.edu>
2020-04-14 09:52:22 -0500

    deps: use config instead of configly

--- config/index.js
+++
@@ -1,4 +0,0 @@
-// Have to disable this rule because we're using a fake
-// `config` module as documented here:
-// https://github.com/alexindigo/configly#migration-from-config
-module.exports = require('configly')(__dirname);
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const request = require('supertest');
 const nock = require('nock');
-const config = require('../config');
+const config = require('config');
 
 const middleware = require('..');
 
